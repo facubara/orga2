e2bf9dda7550b5c5c8cd5a802ef27e2bacc09c59
jiro789 <iansdens@gmail.com>
2015-05-03 23:29:50 -0300

    aca va todo

--- TP2/entregable/tp2-bundle.v2/src/filters/ASM_blur1.asm
+++ TP2/entregable/tp2-bundle.v2/src/filters/ASM_blur1.asm
@@ -8,13 +8,15 @@ extern malloc
 extern free
 section .data
 constante: times 4 dd 9
+mascara: dd 0xFFFFFFFF,0xFFFFFFFF,0xFFFFFFFF,0
+mascara2: db 0,0,0,0,0xff,0xff,0xff,0,0,0,0,0,0,0,0,0
 section .text
 ; void ASM_blur1( uint32_t w, uint32_t h, uint8_t* data )
 global ASM_blur1
 ASM_blur1:
     
     ;rdi = ancho
-    ;rsi = alto
+    ;rsi = altos
     ;rdx = src
     push rbp
     mov rbp, rsp
@@ -22,10 +24,9 @@ ASM_blur1:
     push r13
     push r14
     push r15
+    push rbx
+    sub rsp,8
 
-    xor r12, r12    ;contador filas
-    xor r13, r13    ;contador columnas
-    xor r14, r14
     xor r15, r15
     xor r8, r8
     xor r9, r9
@@ -36,195 +37,138 @@ ASM_blur1:
     pxor xmm4, xmm4
     pxor xmm5, xmm5
     pxor xmm6, xmm6
-    pxor xmm7, xmm7
-    pxor xmm8, xmm8
-    pxor xmm9, xmm9
-    pxor xmm10, xmm10
-    mov qword r9, rdi 
-    shl r9, 2                   ;cantidad de bytes por fila, suponiendo que venia en pixels
-    mov qword r10, r9
-    mov qword rax, r9
-    mul rsi                    ;pixels en cada fila * cant filas = tamaño imagen
-    mov qword rdi, rax
-    mov qword r9, rdi           ;respaldo tamaño imagen
-    mov qword r8, rsi           ;respaldo altura
-    call malloc                 ;pido memoria para la copia de la imagen
-    mov qword rcx, rax
-    mov qword r11, rdx
+    ; Fin de seteo 
+    mov r13, rdx                  ;backup puntero
+    mov r14, rdi                  ;backup  columnas
+    mov r15,rsi                   ;backup filas
+
+    mov rax, rdi          ;preparo mul 
+    mul rsi               ;rdi*rsi Columnas* filas
+    lea rax,[rax*4]         ;multiplicacion de ancho*alto*4 (tamanio bytes)
+    mov rdi, rax                   ; preparo malloc
+    call malloc                   ; tengo en rax el puntero a copy
+    shr r9, 4                     ;tengo el tamanio de imagen en grupos de  4 pixeles
+    mov rdi, r13                  ; rdi puntero al dato rdi = r13
+    mov rsi, rax                  ; puntero a copy rsi = rax
+
     .ciclocopia:
-    cmp r15, r9               ;termine de copiar?
-    jz .fincopia
-    mov byte r14, [rdx]
-    mov byte [rax], r14b
+    ; en r9 = tamanio de imagen, en rsi puntero a la copia 
+    cmp r15, r9                       ;termine de copiar?
+    jz .cargarVar
+    movdqu xmm0, [rdi]
+    movdqu xmm0, [rsi]
     inc r15                           ;copio lo que estaba en la imagen
-    inc rdx
-    inc rax
+    lea rdx,[rdx+16]
+    lea rdi,[rdi+16]
     jmp .ciclocopia
-  .fincopia:                         ;tengo en rcx el puntero a la nueva imagen copiada
-    mov qword rdx, r11               ;reestablezco el puntero en el inicio de la imagen original
-    mov qword rsi, r8                ;reestablezco cantidad de filas
-    sub r8, 1                    ;filas -2 para saber si estoy en borde despues
-    mov qword r9, rdi 
-    shl r9, 2                     ;cantidad de bytes por fila, suponiendo que venia en pixels
-    mov qword r10, r9
-    sub r10, 1                          ;cantidad de bytes por fila - 2 para saber si estoy en el borde
-    mov qword r11, r9                ;voy a usar r11 para moverme por src
+    ; la copia la hago contando las iteraciones y luego copiando esa cantidad de veces
+  .cargarVar:                        
     movdqu xmm14, [constante]         ;xmm14 = 9 | 9 | 9 | 9 
     cvtdq2ps xmm14, xmm14             ;xmm14 = 9 | 9 | 9 | 9 (en float)
-    mov rax, rcx                      ;guardo puntero a la imagen copia para hacer free despues
+    mov r8, r14       
+    sub r8,2                          ;contador columna
+    mov r9, r15                       
+    sub r9,2                          ;contador fila
+    mov rsi, rax                      ;puntero a la fila actual (me muevo en la copia)
+    lea rsi, [rsi+r14*4+4]            ;pongo la segunda fila segundo pixel como inicio
+    mov rdi, rsi                      ;puntero a mi pixeles a cargar ->|v|pixel|v|v| (memoria)
+    movdqu xmm15, [mascara]           ;cargo en xmm15 la mascara para and
+    xor rdx,rdx
+    xor rcx,rcx
+    mov r12, rax                      ;backup rax
+    mov rax, r13                      ;puse en rax puntero a la orignal
+    lea rax, [rax+r14*4+4]            ;pongo el puntero original tmb en segunda fila segundo pixel
   .ciclofila:
-     cmp r12, 1         ;fila < 2?
-     jl .avanzofila
-     cmp r12, r8         ;fila >= cantfilas - 2?
-     jge .avanzofila 
-     xor r13, r13          ;reseto contador columnas
-     cmp r12, rsi
-     jz .fin
-  .ciclocolumna:
-      cmp r13, 1         ;columna <2?
-      jl .avanzocol      ;dejo el pixel como estaba
-      cmp r13, r10       ;columna >= tamaño col -2 ?
-      jge .avanzocol     ;dejo el pixel como estaba
-      ;movd xmm0, [rcx]
+     cmp rdx, r9           ;se fija si termine con las filas   
+     je .fin               ;si es la ultima termino
+     xor rcx, rcx          ;reseto contador columnas
+  .cicloColumna:
+      cmp rcx, r8                       ;se fija si termine con las columnas
+      je .avanzoFila                    ;si termine con las columnas salto a la otra
+     
 
-        ;MOVD MUEVE EXACTAMENTE 4 BYTES, OSEA UN PIXEL A LA PARTE BAJA DE XMM0
-                                ; PROCESO P1
-				;movd xmm1,xmm0
-                                movd xmm1, [rcx]
-				punpcklbw xmm1,xmm15
-				punpcklwd xmm1,xmm15
-				;cvtdq2ps xmm1,xmm1			; xmm1 = p1_b | p1_g | p1_r | p1_a  (como floats)
-				;cvttps2dq xmm1,xmm1			; paso a int truncado
-				
+      ;EMPIEZA OPERATORIA CON PIXEL
+      movdqu xmm0,[rdi]                 ;cargo el pixel a cargar
+      mov rbx, rdi                      ;rbx = rdi
+      mov r12, r14                      ;r12 = r14
+      lea r12,[r12*4]                   ;r12*4 (offset fila)
+      sub rbx, r12                      ;rbx estoy en la fila superior
+      movdqu xmm1, [rbx]                ; cargo vecinos superiores
+      lea rbx, [rbx + r14 * 8]          ; estoy en la fila inferior
+      movdqu xmm2, [rbx]                ; cargo vecinos inferiores
+      ;xmm0 = |p3|p2|t|p0| medio
+      ;xmm1 = |s3|s2|s1|s0| superior
+      ;xmm2 = |i3|i2|i1|i0| inferior
+      pand xmm0, xmm15                ; hago cero la basura
+      pand xmm1, xmm15                ; hago cero la basura
+      pand xmm2, xmm15                ; hago cero la basura
+      ;xmm0 = |0|p2|t|p0| medio
+      ;xmm1 = |0|s2|s1|s0| superior
+      ;xmm2 = |0|i2|i1|i0| inferior
+      movdqu xmm3, xmm0               ;preparo para unpack xmm0          
+      movdqu xmm4, xmm1               ;preparo para unpack xmm1
+      movdqu xmm5, xmm2               ;preparo para unpack xmm2
+      punpcklbw xmm0,xmm6
+      punpckhbw xmm3,xmm6             ;despempaquete m en xmm0 y xmm3 
+      punpcklbw xmm1,xmm6
+      punpckhbw xmm4,xmm6             ;despempaquete s en xmm1 y xmm4 
+      punpcklbw xmm2,xmm6 
+      punpckhbw xmm5,xmm6             ;despempaquete i en xmm2 y xmm5 
+      ;ahora tengo todo de a dos falta sumarlos 
+      paddw xmm0,xmm3                 ;xmm0 = t|p0+p2
+      paddw xmm1,xmm4                 ;xmm1 = s1|s0+s2
+      paddw xmm2,xmm5                 ;xmm2 = i1|i0+i2
+      paddw xmm0,xmm1                 ;xmm0 = t+s1|p0+p2+s0+s2
+      paddw xmm0,xmm2                 ;xmm0 = t+s1+i1|p0+p2+s0+s2+i0+i2
+      movdqu xmm1, xmm0               ;xmm1 = xmm0
+      ;movdqu xmm3, [shift4]          ;cargo la mascara 
+      ;psrldq xmm1, xmm3               ;shifteo 4 bytes hacia la derecha xmm1 = 0|t+s1+i1 
+      ;paddb xmm0, xmm1                ;xmm0 = t+s1+i1|t+s1+i1+p0+p2+s0+s2+i0+i2  ya sume todo
+      punpcklwd xmm0,xmm6             ;desempaquete xmm0 = p0+p2+s0+s2+i0+i2
+      punpckhwd xmm1,xmm6             ;desmpaquete  xmm1 = t+s1+i1
+      paddd xmm0, xmm1                ;xmm0 = p0+p2+s0+s2+i0+i2+t+s1+i1 Toda la suma entera 
+      cvtdq2ps xmm0,xmm0              ;paso a float cada una de las componentes 
+      divps xmm0, xmm14               ;dividi toda las componentes por 9 
+      cvtps2dq xmm0,xmm0              ;los volvi a pasar a integer
+      packssdw xmm0,xmm0              ;los paso a word
+      packsswb xmm0,xmm0              ;los paso a byte xmm0 = |x|x|x|t
+      
+      ;parche 
+      mov rbx, rdi                        ;backup rdi
+      mov r10, rax                        ;backup rax 
+      mov rax, r9                         ;rax = filaQmeMovi 
+      mul r14                             ;rax = filaQmeMovi*cantColumna
+      lea rax,[rax*4]                     ;rax = filaQmeMovi*cantColumna*4 (cuantos bytes me movi de filas)
+      mov rdi,r10                         ;pongo el puntero a la segunda fila segunda segundo pixel de la original
+      add rdi, rax                        ;le agrego el offsetFila al puntero
+      lea rdi,[rdi+r8*4]                  ;puntero_a_la_segunda_fila+offsetFila+cantColQMeMovi*4(offset columna) calculo la posicion del pixel que levanto en la original
+      mov rax,r10                         ;devuelvo rax a lo original
+      ;falta volver rdi al original
+      pslldq xmm0, 0x04              ;shifteo 4 bytes hacia la izq xmm0 = |x|x|t|0 
+      movdqu xmm3, [mascara2]         ;cargo la mascara
+      maskmovdqu xmm0, xmm3           ;esto va cargar los bytes que su bit mas significativo empieze con 1 cargando a memoria 
+      ;si mi mascara esta bien deberia cargar cargar solo los r g b del pixel t
+      mov rdi,rbx                     ;ahi lo volvi al orignial 
+      ;TERMINO OPERATORIA CON PIXEL
 
-                                ; "LOS DE ARRIBA"
-				mov r11,rcx
-				sub r11,r9					; le resto src_row_size ahora estoy en la fila i-1	
-				sub r11,4					; corro j 1 lugar
-				
-				movd xmm2,[r11]
-				punpcklbw xmm2,xmm15
-				punpcklwd xmm2,xmm15
-				;cvtdq2ps xmm2,xmm2			; xmm2 = componentes del px de (i-1,j-1) (como floats)
-				;cvttps2dq xmm2,xmm2			; paso a int truncado
-				
-				mov r11,rcx
-				sub r11,r9					; pongo r11 en la fila anterior
-	
-				movd xmm3,[r11]
-				punpcklbw xmm3,xmm15
-				punpcklwd xmm3,xmm15
-				;cvtdq2ps xmm3,xmm3			; xmm3 = componentes del px de (i-1,j) (como floats)
-				;cvttps2dq xmm3,xmm3			; paso a int truncado
-				
-				mov r11,rcx
-				sub r11, r9                              ;fila anterior
-                                add r11, 4                               ;pixel siguiente
 
-				movd xmm4,[r11]
-				punpcklbw xmm4,xmm15
-				punpcklwd xmm4,xmm15
-				;cvtdq2ps xmm4,xmm4			; xmm4 = componentes del px de (i-1,j+1) (como floats)
-				;cvttps2dq xmm4,xmm4			; paso a int truncado
-				
-				mov r11,rcx                              ;misma fila
-                                sub r11, 4                               ;pixel anterior
-				
-				
-				movd xmm5,[r11]
-				punpcklbw xmm5,xmm15
-				punpcklwd xmm5,xmm15
-				;cvtdq2ps xmm5,xmm5			; xmm5 = componentes del px de (i,j-1) (como floats)
-				;cvttps2dq xmm5,xmm5			; paso a int truncado
 
-                                mov r11, rcx                            ;misma fila
-                                add r11, 4                              ;pixel siguiente
-
-                                movd xmm6,[r11]
-				punpcklbw xmm5,xmm15
-				punpcklwd xmm5,xmm15
-				;cvtdq2ps xmm5,xmm5			; xmm5 = componentes del px de (i,j+1) (como floats)
-				;cvttps2dq xmm5,xmm5			; paso a int truncado
-
-                                mov r11, rcx                            ;misma fila
-                                add r11, r9                             ;fila siguiente
-                                sub r11, 4                              ;pixel anterior
-
-                                movd xmm7,[r11]
-				punpcklbw xmm5,xmm15
-				punpcklwd xmm5,xmm15
-				;cvtdq2ps xmm5,xmm5			; xmm5 = componentes del px de (i+1,j-1) (como floats)
-				;cvttps2dq xmm5,xmm5			; paso a int truncado
-
-                                mov r11, rcx				;misma fila
-                                add r11, r9				;fila siguiente mismo pixel
-                                
-                                movd xmm8,[r11]
-				punpcklbw xmm5,xmm15
-				punpcklwd xmm5,xmm15
-				;cvtdq2ps xmm5,xmm5			; xmm5 = componentes del px de (i+1,j) (como floats)
-				;cvttps2dq xmm5,xmm5			; paso a int truncado
-
-                                mov r11, rcx				;misma fila
-                                add r11, r9				;fila siguiente
-                                add r11, 4				;pixel siguiente
-                                
-                                movd xmm9,[r11]
-				punpcklbw xmm5,xmm15
-				punpcklwd xmm5,xmm15
-				;cvtdq2ps xmm5,xmm5	  ;xmm5 = componentes del px de (i+1,j+1) (como floats)
-				;cvttps2dq xmm5,xmm5			; paso a int truncado
-				
-
-                                ;ACA YA TENGO LA MATRIZ ARMADA
-                                
-                                ;paddd xmm0, xmm1
-                                paddd xmm1, xmm2
-				paddd xmm1, xmm3
-				paddd xmm1, xmm4
-				paddd xmm1, xmm5
-				paddd xmm1, xmm6
-				paddd xmm1, xmm7
-			        paddd xmm1, xmm8
-				paddd xmm1, xmm9
-
-                                ;SUMO CON INTS Y DESPUES CONVIERTO A FLOAT PARA DIVIDIR
-
-                                ;addps xmm0, xmm1
-                                ;addps xmm0, xmm2
-				;addps xmm0, xmm3
-				;addps xmm0, xmm4
-				;addps xmm0, xmm5
-				;addps xmm0, xmm6
-				;addps xmm0, xmm7
-				;addps xmm0, xmm8
-				;addps xmm0, xmm9
-				;ACA ESTA LA SUMA EN float en xmm0
-                                cvtdq2ps xmm1, xmm1
-                                divps xmm1, xmm14                ;divido por 9
-                                cvtps2dq xmm1, xmm1              ;paso a int
-
-                         ;COLOCO
-
-                         movd [rdx], xmm1
-                         pxor xmm1, xmm1
-                         pxor xmm0, xmm0
-                       .avanzocol:
-                         add rdx, 4                       ;copie 1 pixel mas a la original
-                         add rcx, 4                       ;copie 1 pixel mas de la copia
-                         inc r13
-                         cmp r10, r9                      ;termine con la fila?
-                         jz .avanzofila
-                         jmp .ciclocolumna
-                       .avanzofila:
-                         inc r12 
-                         jmp .ciclofila
+      inc rcx                         ;incremente la columna
+      lea rdi, [rdi + 4]              ;mueve al siguiente el puntero de pixel a cargar
+      jmp .cicloColumna
+      .avanzoFila:
+        inc rdx                       ;incremento contador fila
+        lea rsi,[rsi+ r14*4]          ; rsi+=columnas*4 pongo el puntero fila actual en la siguiente
+        mov rdi, rsi                  ; pongo el puntero pixeles a cargar en el inicio de la fila
+        jmp .ciclofila
                          
                            
 
 .fin:
-  mov qword rdi, rax
-  call free                    ;libero la memoria de la imagen copia
+  mov qword rdi, r12
+  call free                           ;libero la memoria de la imagen copia
+  add rsp,8
+  pop rbp
   pop r15
   pop r14
   pop r13
