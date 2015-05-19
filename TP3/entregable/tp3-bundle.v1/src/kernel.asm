8691951caa8f01592b2c42b4625d92875e9ce28a
Alejandro Ruben Mignanelli <amignanelli@ws20.labo7>
2015-05-19 20:18:24 -0300

    empezando el tp3

---
+++ TP3/entregable/tp3-bundle.v1/src/kernel.asm
@@ -0,0 +1,118 @@
+; ** por compatibilidad se omiten tildes **
+; ==============================================================================
+; TRABAJO PRACTICO 3 - System Programming - ORGANIZACION DE COMPUTADOR II - FCEN
+; ==============================================================================
+
+%include "imprimir.mac"
+
+global start
+
+
+;; Saltear seccion de datos
+jmp start
+
+;;
+;; Seccion de datos.
+;; -------------------------------------------------------------------------- ;;
+iniciando_mr_msg db     'Iniciando kernel (Modo Real)...'
+iniciando_mr_len equ    $ - iniciando_mr_msg
+
+iniciando_mp_msg db     'Iniciando kernel (Modo Protegido)...'
+iniciando_mp_len equ    $ - iniciando_mp_msg
+
+;;
+;; Seccion de código.
+;; -------------------------------------------------------------------------- ;;
+
+;; Punto de entrada del kernel.
+BITS 16
+start:
+    ; Deshabilitar interrupciones
+    cli
+
+    ; Cambiar modo de video a 80 X 50
+    mov ax, 0003h
+    int 10h ; set mode 03h
+    xor bx, bx
+    mov ax, 1112h
+    int 10h ; load 8x8 font
+
+    ; Imprimir mensaje de bienvenida
+    imprimir_texto_mr iniciando_mr_msg, iniciando_mr_len, 0x07, 0, 0
+
+
+    ; Habilitar A20
+
+    call habilitar_A20
+
+    ; Cargar la GDT
+
+    lgdt [GDT_DESC]
+
+    ; Setear el bit PE del registro CR0
+
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+
+    ; Saltar a modo protegido
+
+    jmp 0x08:modo_protegido
+
+
+    ; Establecer selectores de segmentos
+
+  .modo_protegido:
+
+    xor eax, eax
+    mov ax, 1000000b       ; index = 8 / gdt/ldt = 0 / rpl = 0
+    mov ds, ax
+    mov es, ax
+    mov gs, ax
+    mov fs, ax              ; no se esto
+
+    ; Establecer la base de la pila
+
+    ; Imprimir mensaje de bienvenida
+
+    ; Inicializar el juego
+
+    ; Inicializar pantalla
+
+    ; Inicializar el manejador de memoria
+
+    ; Inicializar el directorio de paginas
+
+    ; Cargar directorio de paginas
+
+    ; Habilitar paginacion
+
+    ; Inicializar tss
+
+    ; Inicializar tss de la tarea Idle
+
+    ; Inicializar el scheduler
+
+    ; Inicializar la IDT
+
+    ; Cargar IDT
+
+    ; Configurar controlador de interrupciones
+
+    ; Cargar tarea inicial
+
+    ; Habilitar interrupciones
+
+    ; Saltar a la primera tarea: Idle
+
+    ; Ciclar infinitamente (por si algo sale mal...)
+    mov eax, 0xFFFF
+    mov ebx, 0xFFFF
+    mov ecx, 0xFFFF
+    mov edx, 0xFFFF
+    jmp $
+    jmp $
+
+;; -------------------------------------------------------------------------- ;;
+
+%include "a20.asm"
